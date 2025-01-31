e3c14d1bb3fcb7a61b8f4a2c6c5e7faf1e79b4e2
Khalil <khalilhenane@hotmail.com>
2025-02-01 00:54:22 +0100

    Add pick meal page. And it fetches everytime   after clickt on next button

--- lib/controllers/fetch_meal.dart
+++ lib/controllers/fetch_meal.dart
@@ -5,7 +5,7 @@ import 'dart:convert';
 import 'dart:async';
 import 'package:http/http.dart' as http;
 
-class fetchMealController {
+class FetchMealController {
 //API
 // Not tested yet
   Future<Meal> fetchMeal() async {
@@ -14,6 +14,22 @@ class fetchMealController {
     if (url == null || url == '') {
       throw Exception('Invalid URL');
     }
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+
+      List<dynamic> mealsJson = data['meals'];
+      List<Meal> meals = mealsJson.map((mealJson) => Meal.fromJson(mealJson)).toList();
+      return meals[0];
+    } else {
+      throw Exception('Failed to load meal');
+    }
+  }
+
+  Future<Meal> fetchRandomMeal() async {
+    final url = Uri.parse('https://www.themealdb.com/api/json/v1/1/random.php');
 
     final response = await http.get(url);
 
---
+++ lib/pages/pick_meal.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/controllers/fetch_meal.dart';
+import 'package:flutter_application_1/models/meal_model.dart';
+
+class PickMealPage extends StatefulWidget {
+  const PickMealPage({super.key});
+
+  @override
+  State<PickMealPage> createState() => _PickMealPageState();
+}
+
+class _PickMealPageState extends State<PickMealPage> {
+  Meal? _meal;
+
+  void _fetchMeal() async {
+    final fetchMealController = FetchMealController();
+    final meal = await fetchMealController.fetchRandomMeal();
+    setState(() {
+      _meal = meal;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchMeal();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pick Meal'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 60,
+            ),
+            Text(
+              'Pick Meal',
+              style: TextStyle(fontSize: 26),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: _meal != null ? NetworkImage(_meal!.strMealThumb) : AssetImage('assets/placeholder.png') as ImageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              height: 200,
+              width: 300,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              onPressed: _fetchMeal,
+              child: Text(' Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
